0b45fcd892f5aad624feecfc5d2c2674614d8546
Seçkin Yıldız <syildiznbjk@gmail.com>
2024-03-17 19:30:32 +0300

    .

--- android/app/src/main/kotlin/com/example/flutter32_oua_app_jam24/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter32_oua_app_jam24/MainActivity.kt
@@ -3,5 +3,4 @@ package com.example.flutter32_oua_app_jam24
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    // Buraya herhangi bir şey eklemenize gerek yok.
 }
